b5bf4aa104b024bbd0771e45b1097bdb8f45e623
Robert De Leon <robert.deleon@softwire.com>
2024-07-02 14:34:14 +0100

    debug stmts added

--- emailextraction.js
+++ emailextraction.js
@@ -2,21 +2,24 @@ const fs = require('fs');
 fs.readFile('test2.txt', "utf-8", (err, data) => {
     if (err) throw err;
     const emails = data.toString().split(" ");
+
     let counter = 0;
     const regex = /^[a-zA-Z0-9.'_%+-]+@softwire\.com$/;
   
     for(let i=0; i<emails.length; i++) {
         let email = emails[i];
         let len = email.length;
-        console.log(email);
+        // console.log(email);
           
        // if((email.match(regex))) {
-           if(email.substring(len-13, len) === "@softwire.com") {
-                counter++;
-                console.log("Match",email);
-        }
+       console.log(`len - 13: ${(len - 13)}, len: ${len}`);
+       console.log(`email: ${email}, substring: ${email.substring(len-13, len)}`);
+    //        if(email.substring(len-13, len) === "@softwire.com") {
+    //             counter++;
+    //             console.log("Match",email);
+    //     }
 
     }
-    console.log(counter);
+    // console.log(counter);
 });
 
