029efc209b871f32d15dc070d7e365dc73ef16f9
Robert De Leon <robert.deleon@softwire.com>
2024-07-02 11:59:19 +0100

    Added another for loop for each email address

--- emailextraction.js
+++ emailextraction.js
@@ -3,11 +3,16 @@ fs.readFile('test.txt', (err, data) => {
     if (err) throw err;
     //console.log(data.toString());
     const emails = data.toString().split(" ");
-    console.log(emails);
+    // console.log(emails);
     let counter = 0;
     for(let i=0; i<emails.length; i++) {
-        console.log(emails[i]);
-        //if ((emails.substring(i, 13) === "@softwire.com")) {counter++}
+        // console.log(emails[i]);
+        for (let j = 0; j < emails[i].length; j++) {
+            if ((emails[i].substring(i, 13) === "@softwire.com")) {
+                console.log(emails[i]);
+                counter++;
+            }
+        }
 
     }
     console.log(counter);
